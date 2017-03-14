05b3780f92814fae44d7c96b0bc3457aefb6ea12
CharlesBird <1016784928@qq.com>
2017-03-14 11:38:23 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 import os
 
-
+# 列出给定文件下所有文件树形结构
 def printFile(path, level):
     if os.path.exists(path):
         files = os.listdir(path)
@@ -14,3 +14,6 @@ def printFile(path, level):
                 printFile(subpath, leveli)
 if __name__ == '__main__':
     printFile(r'C:', 1)
+
+# 判断一个字符不少于50字，去除空格换行符
+len(field_value.replace(' ', '').replace('\n', '')) < 50
